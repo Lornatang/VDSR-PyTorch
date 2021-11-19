6720bc7aae4e59f0ffaad3e14f43998060b5199a
Liu Changyu <liuchangyu1111@gmail.com>
2021-11-20 02:23:40 +0800

    Create model.py

---
+++ model.py
@@ -0,0 +1,73 @@
+# Copyright 2021 Dakewe Biotech Corporation. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Realize the model definition function."""
+from math import sqrt
+
+import torch
+from torch import nn
+
+
+class ConvReLU(nn.Module):
+    def __init__(self, channels: int) -> None:
+        super(ConvReLU, self).__init__()
+        self.conv = nn.Conv2d(channels, channels, (3, 3), (1, 1), (1, 1))
+        self.relu = nn.ReLU(True)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        out = self.conv(x)
+        out = self.relu(out)
+
+        return out
+
+
+class VDSR(nn.Module):
+    def __init__(self) -> None:
+        super(VDSR, self).__init__()
+        # Input layer
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 64, (3, 3), (1, 1), (1, 1)),
+            nn.ReLU(True)
+        )
+
+        # Features trunk blocks
+        trunk = []
+        for _ in range(18):
+            trunk.append(ConvReLU(64))
+        self.trunk = nn.Sequential(*trunk)
+
+        # Output layer
+        self.conv2 = nn.Conv2d(64, 1, (3, 3), (1, 1), (1, 1))
+
+        # Initialize model weights
+        self._initialize_weights()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self._forward_impl(x)
+
+    # Support torch.script function
+    def _forward_impl(self, x: torch.Tensor) -> torch.Tensor:
+        identity = x
+
+        out = self.conv1(x)
+        out = self.trunk(out)
+        out = self.conv2(out)
+        out = torch.add(out, identity)
+
+        return out
+
+    def _initialize_weights(self) -> None:
+        for module in self.modules():
+            if isinstance(module, nn.Conv2d):
+                nn.init.normal_(module.weight.data, 0.0, sqrt(2 / (module.out_channels * module.weight.data[0][0].numel())))
+                nn.init.zeros_(module.bias.data)
