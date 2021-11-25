570e7bca43c70df0c25df606d53b987fdd40b670
Liu Changyu <liuchangyu1111@gmail.com>
2021-11-25 15:29:49 +0800

    Update run.py

--- scripts/run.py
+++ scripts/run.py
@@ -8,7 +8,7 @@ os.system("python3 ./split_train_valid_dataset.py --inputs_dir ../data/TB291/VDS
 
 # Create LMDB database file
 os.system("python3 ./create_lmdb_dataset.py --image_dir ../data/TB291/VDSR/train/inputs --lmdb_path ../data/train_lmdb/VDSR/TB291_LR_lmdb")
-os.system("python3 ./create_lmdb_dataset.py --image_dir ../data/TB291/VDSR/train/target --lmdb_path ../data/train_lmdb/VDSR/TB291_LR_lmdb")
+os.system("python3 ./create_lmdb_dataset.py --image_dir ../data/TB291/VDSR/train/target --lmdb_path ../data/train_lmdb/VDSR/TB291_HR_lmdb")
 
 os.system("python3 ./create_lmdb_dataset.py --image_dir ../data/TB291/VDSR/valid/inputs --lmdb_path ../data/valid_lmdb/VDSR/TB291_LR_lmdb")
 os.system("python3 ./create_lmdb_dataset.py --image_dir ../data/TB291/VDSR/valid/target --lmdb_path ../data/valid_lmdb/VDSR/TB291_HR_lmdb")
