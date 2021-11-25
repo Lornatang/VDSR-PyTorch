71eb4725894bd485d7d9982f2500906724b97eaf
Liu Changyu <liuchangyu1111@gmail.com>
2021-11-25 10:23:37 +0800

    Update run.py

--- scripts/run.py
+++ scripts/run.py
@@ -1,14 +1,14 @@
 import os
 
 # Prepare dataset
-os.system("python ./prepare_dataset.py")
+os.system("python3 ./prepare_dataset.py --inputs_dir ../data/TB291/original --output_dir ../data/TB291/VDSR/")
 
 # Split train and valid
-os.system("python ./split_train_valid_dataset.py")
+os.system("python3 ./split_train_valid_dataset.py --inputs_dir ../data/TB291/VDSR")
 
 # Create LMDB database file
-os.system("python ./create_lmdb_dataset.py --image_dir TB291/VDSR/train/inputs --lmdb_path train_lmdb/VDSR/TB291_LR_lmdb")
-os.system("python ./create_lmdb_dataset.py --image_dir TB291/VDSR/train/target --lmdb_path train_lmdb/VDSR/TB291_LR_lmdb")
+os.system("python3 ./create_lmdb_dataset.py --image_dir ../data/TB291/VDSR/train/inputs --lmdb_path ../data/train_lmdb/VDSR/TB291_LR_lmdb")
+os.system("python3 ./create_lmdb_dataset.py --image_dir ../data/TB291/VDSR/train/target --lmdb_path ../data/train_lmdb/VDSR/TB291_LR_lmdb")
 
-os.system("python ./create_lmdb_dataset.py --image_dir TB291/VDSR/valid/inputs --lmdb_path valid_lmdb/VDSR/TB291_LR_lmdb")
-os.system("python ./create_lmdb_dataset.py --image_dir TB291/VDSR/valid/target --lmdb_path valid_lmdb/VDSR/TB291_HR_lmdb")
+os.system("python3 ./create_lmdb_dataset.py --image_dir ../data/TB291/VDSR/valid/inputs --lmdb_path ../data/valid_lmdb/VDSR/TB291_LR_lmdb")
+os.system("python3 ./create_lmdb_dataset.py --image_dir ../data/TB291/VDSR/valid/target --lmdb_path ../data/valid_lmdb/VDSR/TB291_HR_lmdb")
